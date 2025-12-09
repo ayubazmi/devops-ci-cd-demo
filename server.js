4a44e18f4f098a6ce6877cd7b932b99f56d87ea3
ayubazmi <119778643+ayubazmi@users.noreply.github.com>
2025-12-09 23:40:59 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello from CI/CD demo!");
-});
-
-app.listen(3000, () => console.log("Server running on 3000"));
